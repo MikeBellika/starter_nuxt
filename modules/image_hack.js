386535d62aa31715dea3f4d9f72633a77210186d
Mike Bellika <mike@altinget.dk>
2022-07-13 12:05:37 +0200

    root?

--- modules/image_hack.js
+++ modules/image_hack.js
@@ -1,25 +1,31 @@
 export default function ExampleModule(moduleOptions) {
   const fs = require('fs')
-    console.error(this.options.image)
-    console.error('HELLO WORLD')
-    console.error('HELLO WORLD')
-    console.error('HELLO WORLD')
-    console.error('HELLO WORLD')
-    console.error('HELLO WORLD')
-    console.error('HELLO WORLD')
-    console.error('HELLO WORLD')
-    console.error('HELLO WORLD')
-    console.error('HELLO WORLD')
-    fs.mkdir('.output', (err) => {
-      if(err) console.error(err)
-      console.error('Directory created')
-    })
-    fs.writeFile('.output/images-manifest.json', JSON.stringify(this.options.image), (err) => {
-      if(err) console.error(err)
-      console.error('WRITTEN')
-    })
-  
-    this.nuxt.hook('ready', async nuxt => {
-      console.error('Nuxt is ready')
-    })
-  }
+  console.error(this.options.image)
+  console.error('HELLO WORLD')
+  console.error('HELLO WORLD')
+  console.error('HELLO WORLD')
+  console.error('HELLO WORLD')
+  console.error('HELLO WORLD')
+  console.error('HELLO WORLD')
+  console.error('HELLO WORLD')
+  console.error('HELLO WORLD')
+  console.error('HELLO WORLD')
+  fs.mkdir('/.output', (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.error('Directory created')
+  })
+  fs.writeFile('/.output/images-manifest.json', JSON.stringify(this.options.image), (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.error('WRITTEN')
+  })
+
+  this.nuxt.hook('ready', async nuxt => {
+    console.error('Nuxt is ready')
+  })
+}
