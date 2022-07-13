362cbf68e384e47ec1093fda053c89aa486b922d
Mike Bellika <mike@altinget.dk>
2022-07-13 11:52:36 +0200

    more image hack

--- modules/image_hack.js
+++ modules/image_hack.js
@@ -1,4 +1,5 @@
 export default function ExampleModule(moduleOptions) {
+  const fs = require('fs')
     console.log(this.options.image)
     console.log('HELLO WORLD')
     console.log('HELLO WORLD')
@@ -9,6 +10,14 @@ export default function ExampleModule(moduleOptions) {
     console.log('HELLO WORLD')
     console.log('HELLO WORLD')
     console.log('HELLO WORLD')
+    fs.mkdir('.output', (err) => {
+      if(err) throw err
+      console.log('Directory created')
+    })
+    fs.writeFile('.output/images-manifest.json', JSON.stringify(this.options.image), (err) => {
+      if(err) throw err
+      console.log('WRITTEN')
+    })
   
     this.nuxt.hook('ready', async nuxt => {
       console.log('Nuxt is ready')
