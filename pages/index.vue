25d32120a7c410b7130f181bbdb779f62a3314ec
Mike Bellika <mike@altinget.dk>
2022-07-13 09:35:24 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nuxt-img src="https://avatars.githubusercontent.com/u/5175031?s=40&v=4" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
