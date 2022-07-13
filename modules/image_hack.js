d857bbb36258050360a736f783978d908d3fc56f
Mike Bellika <mike@altinget.dk>
2022-07-13 12:02:33 +0200

    error logs

--- modules/image_hack.js
+++ modules/image_hack.js
@@ -1,25 +1,25 @@
 export default function ExampleModule(moduleOptions) {
   const fs = require('fs')
-    console.log(this.options.image)
-    console.log('HELLO WORLD')
-    console.log('HELLO WORLD')
-    console.log('HELLO WORLD')
-    console.log('HELLO WORLD')
-    console.log('HELLO WORLD')
-    console.log('HELLO WORLD')
-    console.log('HELLO WORLD')
-    console.log('HELLO WORLD')
-    console.log('HELLO WORLD')
+    console.error(this.options.image)
+    console.error('HELLO WORLD')
+    console.error('HELLO WORLD')
+    console.error('HELLO WORLD')
+    console.error('HELLO WORLD')
+    console.error('HELLO WORLD')
+    console.error('HELLO WORLD')
+    console.error('HELLO WORLD')
+    console.error('HELLO WORLD')
+    console.error('HELLO WORLD')
     fs.mkdir('.output', (err) => {
       if(err) console.error(err)
-      console.log('Directory created')
+      console.error('Directory created')
     })
     fs.writeFile('.output/images-manifest.json', JSON.stringify(this.options.image), (err) => {
       if(err) console.error(err)
-      console.warn('WRITTEN')
+      console.error('WRITTEN')
     })
   
     this.nuxt.hook('ready', async nuxt => {
-      console.log('Nuxt is ready')
+      console.error('Nuxt is ready')
     })
   }
